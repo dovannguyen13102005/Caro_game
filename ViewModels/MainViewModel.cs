b1f6cfbba779620e813736e2ebaf04ff8322ea57
dovannguyen13102005 <dovannguyen13102005@gmail.com>
2025-09-08 08:59:13 +0700

    update

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using Caro_game.Commands;
 
 namespace Caro_game.ViewModels
@@ -13,6 +15,7 @@ namespace Caro_game.ViewModels
         private bool _isAIEnabled;
         private string _selectedAIMode;
 
+        // Thuộc tính cho cấu hình bảng
         public ObservableCollection<int> RowOptions { get; set; }
         public ObservableCollection<int> ColumnOptions { get; set; }
         public ObservableCollection<string> Players { get; set; }
@@ -56,24 +59,62 @@ namespace Caro_game.ViewModels
             set { _selectedAIMode = value; OnPropertyChanged(); }
         }
 
+        // --- Cài đặt giao diện ---
+        public ObservableCollection<string> Themes { get; set; } =
+            new ObservableCollection<string> { "Dark (mặc định)", "Light" };
+
+        public ObservableCollection<string> PrimaryColors { get; set; } =
+            new ObservableCollection<string> { "Xanh dương", "Tím", "Lục" };
+
+        private string _selectedTheme;
+        public string SelectedTheme
+        {
+            get => _selectedTheme;
+            set { _selectedTheme = value; OnPropertyChanged(); }
+        }
+
+        private string _selectedPrimaryColor;
+        public string SelectedPrimaryColor
+        {
+            get => _selectedPrimaryColor;
+            set { _selectedPrimaryColor = value; OnPropertyChanged(); }
+        }
+
+        private bool _isSoundEnabled;
+        public bool IsSoundEnabled
+        {
+            get => _isSoundEnabled;
+            set { _isSoundEnabled = value; OnPropertyChanged(); }
+        }
+
+        // Commands
         public ICommand StartGameCommand { get; set; }
         public ICommand ExitCommand { get; set; }
+        public ICommand SaveSettingsCommand { get; set; }
 
         public MainViewModel()
         {
+            // Options cho bàn cờ
             RowOptions = new ObservableCollection<int> { 15, 20 };
             ColumnOptions = new ObservableCollection<int> { 18, 35 };
             Players = new ObservableCollection<string> { "X (Bạn)", "O" };
             AIModes = new ObservableCollection<string> { "Dễ", "Khó" };
 
+            // Default
             SelectedRows = 20;
             SelectedColumns = 35;
             FirstPlayer = "X (Bạn)";
             IsAIEnabled = true;
             SelectedAIMode = "Khó";
 
+            SelectedTheme = "Dark (mặc định)";
+            SelectedPrimaryColor = "Xanh dương";
+            IsSoundEnabled = true;
+
+            // Commands
             StartGameCommand = new RelayCommand(StartGame);
-            ExitCommand = new RelayCommand(_ => System.Windows.Application.Current.Shutdown());
+            ExitCommand = new RelayCommand(_ => Application.Current.Shutdown());
+            SaveSettingsCommand = new RelayCommand(_ => SaveSettings());
         }
 
         private void StartGame(object parameter)
@@ -86,6 +127,31 @@ namespace Caro_game.ViewModels
                 IsAIEnabled = this.IsAIEnabled,
                 AIMode = this.SelectedAIMode
             };
+        }
+
+        private void SaveSettings()
+        {
+            // Theme
+            if (SelectedTheme == "Light")
+            {
+                Application.Current.MainWindow.Background = new SolidColorBrush(Colors.WhiteSmoke);
+                Application.Current.Resources["DefaultForeground"] = new SolidColorBrush(Colors.Black);
+            }
+            else // Dark
+            {
+                Application.Current.MainWindow.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0F172A"));
+                Application.Current.Resources["DefaultForeground"] = new SolidColorBrush(Colors.White);
+            }
+
+            // Primary color
+            Color primaryColor = Colors.DeepSkyBlue;
+            if (SelectedPrimaryColor == "Tím") primaryColor = Colors.MediumPurple;
+            else if (SelectedPrimaryColor == "Lục") primaryColor = Colors.MediumSeaGreen;
+
+            Application.Current.Resources["Primary"] = new SolidColorBrush(primaryColor);
+
+            // Thông báo
+            MessageBox.Show("Cài đặt đã được áp dụng!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
